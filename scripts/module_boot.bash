8ed806292fa6f259b460ba34e1def73f2337829d
Eric D <ericdanziger@cmu.edu>
2014-10-30 17:56:34 -0400

    fixed upstart

--- scripts/module_boot.bash
+++ scripts/module_boot.bash
@@ -1,6 +1,8 @@
+#!/bin/bash
+source /opt/ros/indigo/setup.bash
+source /home/oddbot/catkin_ws/devel/setup.bash
 export ROS_MASTER_URI=http://10.0.2.1:11311
 export ROS_HOSTNAME=udoo14
-source ~/catkin_ws/devel/setup.bash
 
 # Remember to change 'lo' to 'eth0'
 address=$(ip addr show eth0 | grep -o '10.0.[[:digit:]].[[:digit:]]' | head -1 |
@@ -12,26 +14,27 @@ awk '{
 # echo $address
 roslaunch oddbot_boot module.launch node_name:=$address
 
-while[1]; do
-address=$(ip addr show eth0 | grep -o '10.0.[[:digit:]].[[:digit:]]' | head -1 |
-awk '{
-	split($1, a, ".")
-	oldsubnet = subnet
-	subnet = a[3]
-	
-}')
-if [oldsubnet != subnet]; then
-#kill old nodes
-if [ "$(ps aux | grep ros | grep -v grep | awk '{print $2}')" ]; then # or $(rosnode list)
-    for i in $(ps aux | grep ros | grep -v grep | awk '{print $2}')
-    do
-        kill -2 $i;
-    done
-fi
+#while[1]
+#do
+#address=$(ip addr show eth0 | grep -o '10.0.[[:digit:]].[[:digit:]]' | head -1 |
+#awk '{
+#	split($1, a, ".")
+#	oldsubnet = subnet
+#	subnet = a[3]
+#	
+#}')
+#if [oldsubnet != subnet]; then
+##kill old nodes
+#if [ "$(ps aux | grep ros | grep -v grep | awk '{print $2}')" ]; then # or $(rosnode list)
+#    for i in $(ps aux | grep ros | grep -v grep | awk '{print $2}')
+#    do
+#        kill -2 $i;
+#    done
+#fi
 
-#launch new ones
-roslaunch oddbot_boot module.launch node_name:=$address;
-fi
+##launch new ones
+#roslaunch oddbot_boot module.launch node_name:=$address;
+#fi
 
-sleep 10;
-done
+#sleep 10;
+#done
