cbdd01fce616847195a5e0ee05983b37eb3ce36f
amarschn <andrew.marschner@gmail.com>
2014-11-04 11:10:20 -0500

    Added arduino code

---
+++ arduino/udoo_motor_control/serial_command.ino
@@ -0,0 +1,24 @@
+#include "serial_command.h"
+
+void serialEvent1() {
+  if (Serial1.available() > 0) {
+    motor_driver_output = Serial1.read();
+    Serial.print("Receive: ");
+    Serial.println(motor_driver_output);
+    if (serial1_buffer_position = SERIAL_BUFFER_LEN) {
+      serial1_buffer_position = 0;
+    } else {
+      serial1_buffer_position++;
+    }
+    serial1_buffer[serial1_buffer_position] = motor_driver_output;
+  }
+}
+
+int readSerial1() {
+  if (Serial1.available() > 0) {
+    return Serial1.read();
+    delay(10);
+  } else {
+    return 0;
+  }
+}
